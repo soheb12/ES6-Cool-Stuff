536ee6a6b4edff9b2dd584bceeb4458704854c12
Sky_Scraper <moinsoheb02@gmail.com>
2019-11-04 12:36:41 +0530

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -11,3 +11,8 @@ s = s.replace(/world|this|soheb|moin/ig, function (s) {
     return myMap[s];
 })
 console.log(s);
+
+/* Defining properties of objects */
+var obj = {};
+Object.defineProperty(obj, "x", {value:0, writable:false});//property is x with a value of 0 and is READONLY
+console.log(obj.x);//To access

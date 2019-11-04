52deeccc17b83115e93f6e5eaaecd1a45a9d0abf
Sky_Scraper <moinsoheb02@gmail.com>
2019-11-04 15:45:30 +0530

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -1,5 +1,18 @@
-/* Replacing particular words of a string with a particular word */
+/* Defining functions inside object */
+var obj = {
+    name: "soheb",
+    college: "vnrvjiet",
+    roll_no: 596,
+    brach: "cse",
+    info: function () {
 
+        return this.name + " is a student of " + this.college + " bearing the roll no " + this.roll_no + " studying in branch " + this.brach;
+    }
+}
+console.log(obj.info());
+//------------------------------------------------------------------------------------------------------------------------
+
+/* Replacing particular words of a string with a particular word */
 var s = "hello world this Is soheb moin"
 var myMap = {
     "world": "universe",
@@ -11,8 +24,43 @@ s = s.replace(/world|this|soheb|moin/ig, function (s) {
     return myMap[s];
 })
 console.log(s);
+//------------------------------------------------------------------------------------------------------------------------
 
 /* Defining properties of objects */
 var obj = {};
 Object.defineProperty(obj, "x", {value:0, writable:false});//property is x with a value of 0 and is READONLY
 console.log(obj.x);//To access
+//------------------------------------------------------------------------------------------------------------------------
+
+/* Function Closures */
+let f = (function () {
+    console.log("Up");
+    let i = 0;
+    return function () {
+        console.log("Down");
+        i++;
+        return i;
+    }
+})();
+console.log(f());
+console.log(f());
+console.log(f());//Up will be printed only once and Down will be printed 3 times
+//------------------------------------------------------------------------------------------------------------------------
+
+/* Object Closures */
+a = {
+    i: 0,
+    info: function () {
+        this.i++;//agar this avoid karna hai tho go for function closures
+        return this.i;
+    }
+}
+console.log(a.info());
+console.log(a.info());
+console.log(a.info());//Here the variable name will be printed as 3
+//------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
